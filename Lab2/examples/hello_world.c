a6f777e805380f11a546061754422c2c9dadcea8
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2018-02-27 01:05:52 +0530

    added a few more statements for testing

--- Lab2/examples/hello_world.c
+++ Lab2/examples/hello_world.c
@@ -1,11 +1,41 @@
-int printf(void);
+void printf(int a, int b);
 int a[4];
 
-int
-main(void)
+int main(void)
 {
   int a;
   printf(a, b);
   printf();
   return 0;
+}
+
+void printf(int a, int b)
+{
+	int c;
+	c = 1;
+	if (c)
+	{
+		c = 0;
+	}
+	
+	if (!c)
+	{
+		c = 1;
+	}
+	else
+	{
+		c = 0;
+	}
+
+	while (!c)
+	{
+		c = 1;
+	}
+
+	do {
+		c = 0;
+	}
+	while(!c);
+
+	return;
 }
