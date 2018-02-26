679e6fbbcdda9267e1124223686edb862806d024
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2018-02-27 02:28:57 +0530

    more statements to cover test cases

--- Lab2/examples/hello_world.c
+++ Lab2/examples/hello_world.c
@@ -1,19 +1,20 @@
-void printf(int a, int b);
-int a[4];
+void printf(int a, int b, int c), scanf(void);
+int a[4], b[10];
+float f1, *f2, f2;
 
 int main(void)
 {
-  int a;
-  printf(a, b);
+  int a, b, c;
+  printf(a, b, c);
   printf();
   return 0;
 }
 
-void printf(int a, int b)
+void printf(int a, int b, int d)
 {
 	int c;
 	c = 1;
-	if (c)
+	if (c+1 == 2 || c - 1 == 0)
 	{
 		c = 0;
 	}
@@ -33,7 +34,7 @@ void printf(int a, int b)
 	}
 
 	do {
-		c = 0;
+		;
 	}
 	while(!c);
 
