10728421584d6af9954b9e959d6a77e05a08975f
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2018-03-01 10:48:41 +0530

    code gen files changed to incorporate llvm headers

--- Lab2/codeGeneration.hpp
+++ Lab2/codeGeneration.hpp
@@ -1,30 +1,11 @@
 #ifndef CODEGENERATION_H
 #define CODEGENERATION_H
 
-// #include "llvm/ADT/APFloat.h"
-// #include "llvm/ADT/STLExtras.h"
-// #include "llvm/IR/BasicBlock.h"
-// #include "llvm/IR/Constants.h"
-// #include "llvm/IR/DerivedTypes.h"
-// #include "llvm/IR/Function.h"
-// #include "llvm/IR/IRBuilder.h"
-// #include "llvm/IR/LLVMContext.h"
-// #include "llvm/IR/Module.h"
-// #include "llvm/IR/Type.h"
-// #include "llvm/IR/Verifier.h"
-
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Verifier.h"
-
 #include <map>
 #include <string>
 
+#include "llvmHeaders.hpp"
 #include "treeNode.hpp"
-
-using namespace llvm;
 
 // static LLVMContext TheContext;
 static IRBuilder<> Builder(getGlobalContext());
